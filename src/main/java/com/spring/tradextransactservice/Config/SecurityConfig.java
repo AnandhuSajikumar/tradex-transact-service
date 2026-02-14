c5badab11699107ce4e142c3b2d7caaf1cebfa35
anandhu <anandhuzz916@gmail.com>
2026-02-14 19:30:33 +0530

    securityconfig-1

--- src/main/java/com/spring/tradextransactservice/Config/SecurityConfig.java
+++ src/main/java/com/spring/tradextransactservice/Config/SecurityConfig.java
@@ -1,4 +1,53 @@
 package com.spring.tradextransactservice.Config;
 
+import com.spring.tradextransactservice.Filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html")
+                        .permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole("Admin")
+                        .anyRequest().hasRole("USER")
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+//                .exceptionHandling(ex -> ex
+//                        .authenticationEntryPoint((req, res, e) ->
+//                                res.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+//                        )
+//                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
 }
