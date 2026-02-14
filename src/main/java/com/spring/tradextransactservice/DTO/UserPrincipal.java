8eaffd1e229bd65348eccf4c88da04dfbaa14cac
anandhu <anandhuzz916@gmail.com>
2026-02-14 19:29:59 +0530

    jwt

--- src/main/java/com/spring/tradextransactservice/DTO/UserPrincipal.java
+++ src/main/java/com/spring/tradextransactservice/DTO/UserPrincipal.java
@@ -1,4 +1,57 @@
 package com.spring.tradextransactservice.DTO;
 
-public class UserPrinciple {
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class UserPrincipal implements UserDetails {
+    private final Long userId;
+    private final String email;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(Long userId, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public long getId(){
+        return userId;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return getUsername();
+    }
+
+    @Override
+    public String getPassword(){
+        return getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/spring/tradextransactservice/Service/JwtService.java
+++ src/main/java/com/spring/tradextransactservice/Service/JwtService.java
@@ -1,4 +1,47 @@
 package com.spring.tradextransactservice.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return extractUsername(token).equals(userDetails.getUsername())
+                && !isExpired(token);
+    }
+
+    public boolean isExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
