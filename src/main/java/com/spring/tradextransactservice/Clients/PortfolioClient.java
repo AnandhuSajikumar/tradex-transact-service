81a494b0c1a5751030855afb2dbf6f60ddbdfb06
anandhu <anandhuzz916@gmail.com>
2026-02-15 17:13:48 +0530

    PortfolioClient

---
+++ src/main/java/com/spring/tradextransactservice/Clients/PortfolioClient.java
@@ -0,0 +1,35 @@
+package com.spring.tradextransactservice.Clients;
+
+import com.spring.tradextransactservice.DTO.PortfolioUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+
+@Component
+@RequiredArgsConstructor
+public class PortfolioClient {
+
+    private final RestTemplate restTemplate;
+
+    public void updateBuy(Long userId, Long stockId, Integer qty, BigDecimal price){
+        PortfolioUpdateRequest request = new PortfolioUpdateRequest(userId, stockId, qty, price);
+
+        restTemplate.postForObject(
+                "http://localhost:8082/internal/portfolio/buy",
+                request,
+                void.class
+        );
+    }
+
+    public void updateSell(Long userId, Long stockID, Integer qty){
+        var request = new PortfolioUpdateRequest(userId, stockID, qty, null);
+
+        restTemplate.postForObject(
+                "http://localhost:8082/internal/portfolio/buy",
+                request,
+                void.class
+        );
+    }
+}
