b2dc6aebb2ff299f13095934d6d36e73276513d4
anandhu <anandhuzz916@gmail.com>
2026-02-21 14:44:20 +0530

    setup web client

--- src/main/java/com/spring/tradextransactservice/Clients/PortfolioClient.java
+++ src/main/java/com/spring/tradextransactservice/Clients/PortfolioClient.java
@@ -2,7 +2,7 @@ package com.spring.tradextransactservice.Clients;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.math.BigDecimal;
 
@@ -10,25 +10,23 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class PortfolioClient {
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public void updateBuy(Long userId, Long stockId, Integer qty, BigDecimal price){
-        PortfolioUpdateRequest request = new PortfolioUpdateRequest(userId, stockId, qty, price);
-
-        restTemplate.postForObject(
-                "http://localhost:8082/internal/portfolio/buy",
-                request,
-                void.class
-        );
+    public void updateBuy(Long userId, Long stockId, Integer quantity, BigDecimal price) {
+        webClient.post()
+                .uri("http://localhost:8083/portfolio/buy")
+                .bodyValue(new PortfolioUpdateRequest(userId, stockId, quantity, price))
+                .retrieve()
+                .toBodilessEntity()
+                .block();
     }
 
-    public void updateSell(Long userId, Long stockID, Integer qty){
-        var request = new PortfolioUpdateRequest(userId, stockID, qty, null);
-
-        restTemplate.postForObject(
-                "http://localhost:8082/internal/portfolio/buy",
-                request,
-                void.class
-        );
+    public void updateSell(Long userId, Long stockId, Integer quantity) {
+        webClient.post()
+                .uri("http://localhost:8083/portfolio/sell")
+                .bodyValue(new PortfolioUpdateRequest(userId, stockId, quantity, null))
+                .retrieve()
+                .toBodilessEntity()
+                .block();
     }
 }
